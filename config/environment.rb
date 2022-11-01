a0eaec167347f9a6db99d36774ba74f818b80733
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-11-02 00:53:13 +0300

    completed phase 3 active record translating from orm to ar

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,15 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
